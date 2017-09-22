cc032c9d5da588ab07e815797fa382d7146f7ee9
Kinrany <kinrany@yandex.com>
2017-09-22 20:44:34 +0300

    added lab 2

---
+++ js/lab2.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $.getJSON("lab2/questions.json").done(function (data) {
+        const {questions} = data;
+
+        const inputQuestionTemplate = $("#input-question-template").html();
+        const inputQuestionList = $("#input-question-list");
+
+        for (let qi = 0; qi < questions.length; ++qi) {
+            let { question, answers } = questions[qi];
+
+            inputQuestionList.append(inputQuestionTemplate);
+            let inputQuestionItem = inputQuestionList
+                .children(".input-question-item:last-child");
+
+            inputQuestionItem.find(".input-question-label").text(question);
+
+            let inputQuestionSelect = inputQuestionItem.find(".input-question-select");
+            for (let ai = 0; ai < answers.length; ++ai) {
+                let option = $("<option>");
+                option.text(answers[ai]);
+                option.appendTo(inputQuestionSelect);
+            }
+        }
+    });
+});
