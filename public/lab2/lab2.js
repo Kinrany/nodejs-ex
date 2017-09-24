1d69b0326b1191008cdfd6f6c0d2130c36750bc1
Kinrany <kinrany@yandex.com>
2017-09-25 02:42:24 +0300

    fixed lab2

--- public/lab2/lab2.js
+++ public/lab2/lab2.js
@@ -1,30 +1,51 @@
 let questionsJsonUrl = "questions.json";
-let dataPromise = $.getJSON(questionsJsonUrl);
+let dataDeferred = $.getJSON(questionsJsonUrl);
+
+let inputQuestionTemplate;
+let inputForm;
 
 $(document).ready(function () {
-    dataPromise.done(loadQuestions);
+    inputQuestionTemplate = $("#input-question-template").html();
+    inputForm = $("#input-form");
+    
+    dataDeferred.done(loadQuestions);
 });
 
 function loadQuestions(data) {
     const {questions} = data;
 
-    const inputQuestionTemplate = $("#input-question-template").html();
-    const inputQuestionList = $("#input-question-list");
-
     for (let qi = 0; qi < questions.length; ++qi) {
         let { question, answers } = questions[qi];
-
-        inputQuestionList.append(inputQuestionTemplate);
-        let inputQuestionItem = inputQuestionList
-            .children(".input-question-item:last-child");
-
-        inputQuestionItem.find(".input-question-label").text(question);
-
-        let inputQuestionSelect = inputQuestionItem.find(".input-question-select");
-        for (let ai = 0; ai < answers.length; ++ai) {
-            let option = $("<option>");
-            option.text(answers[ai]);
-            option.appendTo(inputQuestionSelect);
-        }
+        appendQuestion(inputForm, question, answers);
     }
+
+    // http://materializecss.com/forms.html#select-initialization
+    $('select').material_select();
+}
+
+function appendQuestion(parent, question, answers) {
+    parent.append(inputQuestionTemplate);
+    let questionItem = parent.children(".input-question-item:last-child");
+
+    questionItem.find(".input-question-label").text(question);
+
+    let questionSelect = questionItem.find(".input-question-select");
+
+    appendOptionDefault(questionSelect, "Выберите вариант");
+    
+    for (let ai = 0; ai < answers.length; ++ai) {
+        appendOption(questionSelect, answers[ai]);
+    }
+}
+
+function appendOption(parent, answer) {
+    let option = $("<option>");
+    option.text(answer);
+    option.appendTo(parent);
+    return option;
+}
+
+function appendOptionDefault(parent, answer) {
+    let option = appendOption(parent, answer);
+    option.addClass('disabled selected');
 }
