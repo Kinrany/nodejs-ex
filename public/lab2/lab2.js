36fc391b7769ca4d9705a79f006bb8fc54d57548
Kinrany <kinrany@yandex.com>
2017-09-25 01:42:14 +0300

    changed lab2.js

--- public/lab2/lab2.js
+++ public/lab2/lab2.js
@@ -1,27 +1,30 @@
 let questionsJsonUrl = "questions.json";
+let dataPromise = $.getJSON(questionsJsonUrl);
 
 $(document).ready(function () {
-    $.getJSON(questionsJsonUrl).done(function (data) {
-        const {questions} = data;
-
-        const inputQuestionTemplate = $("#input-question-template").html();
-        const inputQuestionList = $("#input-question-list");
-
-        for (let qi = 0; qi < questions.length; ++qi) {
-            let { question, answers } = questions[qi];
-
-            inputQuestionList.append(inputQuestionTemplate);
-            let inputQuestionItem = inputQuestionList
-                .children(".input-question-item:last-child");
-
-            inputQuestionItem.find(".input-question-label").text(question);
-
-            let inputQuestionSelect = inputQuestionItem.find(".input-question-select");
-            for (let ai = 0; ai < answers.length; ++ai) {
-                let option = $("<option>");
-                option.text(answers[ai]);
-                option.appendTo(inputQuestionSelect);
-            }
-        }
-    });
+    dataPromise.done(loadQuestions);
 });
+
+function loadQuestions(data) {
+    const {questions} = data;
+
+    const inputQuestionTemplate = $("#input-question-template").html();
+    const inputQuestionList = $("#input-question-list");
+
+    for (let qi = 0; qi < questions.length; ++qi) {
+        let { question, answers } = questions[qi];
+
+        inputQuestionList.append(inputQuestionTemplate);
+        let inputQuestionItem = inputQuestionList
+            .children(".input-question-item:last-child");
+
+        inputQuestionItem.find(".input-question-label").text(question);
+
+        let inputQuestionSelect = inputQuestionItem.find(".input-question-select");
+        for (let ai = 0; ai < answers.length; ++ai) {
+            let option = $("<option>");
+            option.text(answers[ai]);
+            option.appendTo(inputQuestionSelect);
+        }
+    }
+}
