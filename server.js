c1fea58a220c251adc8cbbc666dc89420700e69f
Kinrany <kinrany@yandex.com>
2017-09-30 02:50:21 +0300

    fix 3

--- server.js
+++ server.js
@@ -20,14 +20,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
   mongoURLLabel = "";
 
-if (process.env.OPENSHIFT_APP_NAME) {
-  console.log("OpenShift detected.");
-}
-else {
-  console.log("Setting mongoURL to local path.");
-  mongoURL = "mongodb://localhost:27017/local";
-}
-
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
     mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
@@ -47,6 +39,9 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 
   }
 }
+
+mongoURL = mongoURL || "mongodb://localhost:27017/local";
+
 var db = null,
   dbDetails = new Object();
 
